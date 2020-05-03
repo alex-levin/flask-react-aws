8ba90e9fd74238574dedc62d62a6d4a311e592df
alex-levin <alexlevinb@gmail.com>
2020-05-03 15:41:46 -0400

    replacing flask_restx with Flask app.route decorator

--- services/users/project/api/users/views.py
+++ services/users/project/api/users/views.py
@@ -32,7 +32,7 @@ user = users_namespace.model(
 )
 
 
-@users_blueprint.route('/api/users', methods=['GET'])
+@users_blueprint.route('/api/users/', methods=['GET'])
 def get_users():
     """Get all users"""
     data = []
@@ -41,6 +41,75 @@ def get_users():
         data.append(row.to_json())
     response = jsonify(data)
     return response
+
+
+@users_blueprint.route('/api/users/', methods=['POST'])
+def post_user():
+        post_data = request.get_json()
+        username = post_data.get("username")
+        email = post_data.get("email")
+        password = post_data.get("password")
+        response_object = {}
+
+        user = get_user_by_email(email)
+        if user:
+            response_object["message"] = "Sorry. That email already exists."
+            return response_object, 400
+        add_user(username, email, password)
+        response_object["message"] = f"{email} was added!"
+        return response_object, 201
+
+
+@users_blueprint.route('/api/users/<email>', methods=['GET'])    
+def get_email(email):
+    response_object = {}
+    user = get_user_by_email(email)
+    if not user:
+        response_object["message"] = f"{email} was not found!"
+        return response_object, 404
+    data = user.to_json()
+    response = jsonify(data)
+    return response
+
+
+@users_blueprint.route('/api/users/<int:id>', methods=['GET'])    
+def get_id(id):
+    user = get_user_by_id(id)
+    if not user:
+        response_object["message"] = f"{id} was not found!"
+        return response_object, 404
+    data = user.to_json()
+    response = jsonify(data)
+    return response
+
+
+@users_blueprint.route('/api/users/<int:id>', methods=['PUT'])    
+def put_id(id):
+    put_data = request.get_json()
+    username = put_data.get("username")
+    email = put_data.get("email")
+
+    response_object = {}
+    user = get_user_by_id(id)
+    if not user:
+        response_object["message"] = f"{user.id} was not found!"
+        return response_object, 404
+    
+    update_user(user, username, email)
+    response_object["message"] = f"{user.id} was updated!"
+    return response_object, 200
+
+
+@users_blueprint.route('/api/users/<int:id>', methods=['DELETE'])
+def delete_id(id):
+    response_object = {}
+    user = get_user_by_id(id)
+    if not user:
+        response_object["message"] = f"{user.id} was not found!"
+        return response_object, 404    
+    delete_user(user)
+    response_object["message"] = f"{user.id} was removed!"
+    return response_object, 200    
 
 
 user_post = users_namespace.inherit(
